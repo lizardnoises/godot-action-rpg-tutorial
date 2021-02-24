0f21fe6fe3802c0c25fca35e5dcf52724db32fec
Sean Moore <sean.m.moore@gmail.com>
2021-02-24 14:18:23 -0800

    Simplify player movement code and fix delta scaling bug.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,27 +1,25 @@
 extends KinematicBody2D
 
-const ACCELERATION = 10
-const FRICTION = 10
-const MAX_SPEED = 100
+const ACCELERATION = 500
+const FRICTION = 500
+const MAX_SPEED = 80
 
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = (
-		Input.get_action_strength("ui_right") -
-		Input.get_action_strength("ui_left")
-	)
+		Input.get_action_strength("ui_right")
+		- Input.get_action_strength("ui_left"))
 	input_vector.y = (
-		Input.get_action_strength("ui_down") -
-		Input.get_action_strength("ui_up")
-	)
+		Input.get_action_strength("ui_down")
+		- Input.get_action_strength("ui_up"))
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		velocity = velocity.move_toward(
+			input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta)
