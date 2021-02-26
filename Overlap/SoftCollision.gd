c4bbbf115a588007f966ee50adaebdaf9acde5fc
Sean Moore <sean.m.moore@gmail.com>
2021-02-25 20:51:21 -0800

    Add enemy soft colliding.

---
+++ Overlap/SoftCollision.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func is_colliding():
+	return get_overlapping_areas().size() > 0
+
+func get_push_vector():
+	var push_vector = Vector2.ZERO
+	if is_colliding():
+		push_vector = get_overlapping_areas()[0]\
+			.global_position\
+			.direction_to(global_position)\
+			.normalized()
+	return push_vector
